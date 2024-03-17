652ca7c1ec9b55fbfa805800223b6a97a62d5f5b
yakov23 <yakovoberderfer13@gmail.com>
2024-03-17 15:43:11 +0300

    initial commit

---
+++ src/test/java/ru/netology/rest/postmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class postmanEchoTest {
+
+    @Test
+    void shouldReturnSendDate() {
+        String text = "hi";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body(text)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo(text));
+
+    }
+}
